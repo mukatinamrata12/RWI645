b8bad18538086dd737f02efe8b6477ae57d164d1
mukatinamrata12 <mukatinamrata855@gmail.com>
2024-05-22 22:40:58 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package basics;
+
+/**
+ *
+ * @author Namrata mukati
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
